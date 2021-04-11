98f0babe13ad586e7a2e50db7391cffd35416a36
ejd5mm <ejd5mm@virginia.edu>
2021-04-11 14:02:14 -0400

    added comments

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(shiny)
 
 function(input, output) {
     #reading in the data and basic data cleaning 
-    df <- read.csv("world-happiness-report.csv")
+    df <- read.csv("world-happiness-report.csv") #reading in the data
     df <- df %>%  #changing column names 
         rename(
             Year = year,
@@ -22,18 +22,17 @@ function(input, output) {
             Positive_Affect = Positive.affect,
             Negative_Affect = Negative.affect
         )
-    
+    #manipulating dataset by creating a "Continent" Column from Pre-existing "Country" column 
     df<- df %>%
         mutate(Continent = countrycode(Country, 'country.name', 'continent')) %>%   
         group_by(Continent)
     
-    df <- na.omit(df) 
+    df <- na.omit(df)  #removing NA's
     #View(df)
     data<- df
     
-    # Filter data based on selections
+    # Filter data based on user selections
     output$table <- DT::renderDataTable(DT::datatable({
-        
         data <- df %>%
             filter(
                 if(input$year != "All") {
@@ -54,7 +53,7 @@ function(input, output) {
         return(data)
         
     }))
-        # Generate a summary of the dataset ----
+        # Generate a summary of the dataset (on the left panel)
         output$summary <- renderPrint({
             data <- df %>%
                 filter(
@@ -77,6 +76,7 @@ function(input, output) {
             
         })
     
+       #Generate a function to show the number of rows w/ any given dataframe selection/restriction
     rows = function() {
         data <- df %>%
             filter(
@@ -95,9 +95,10 @@ function(input, output) {
                 } else {TRUE}
             )
         
-        return(nrow(data))
+        return(nrow(data)) #returns number of rows of the data
     }
     
+    #Generate a function to show the number of columns w/ any given dataframe selection/restriction
     cols = function() {
         data <- df %>%
             filter(
@@ -116,24 +117,22 @@ function(input, output) {
                 } else {TRUE}
             )
         
-        return(ncol(data))
+        return(ncol(data)) #returns the number of columns of the data
     }
-    
+
+    #Using the functions created above
         output$columns  <- renderText({
-            paste("Number of Columns:" , cols() )
+            paste("Number of Columns:" , cols() ) #text to display the number of columns
         })
         output$rows  <- renderText({
-            paste("Number of Rows:" , rows() )
+            paste("Number of Rows (Records):" , rows() ) #text to display the number of rows 
         })
 
         output$data_ex  <- renderText({
-            paste("Please see README.md file for information regarding the dataset.")
+            paste("Please see README.md file for information regarding the dataset.") #text to display where to find more information
         })
  
-        # Downloadable csv of selected dataset ----
-        
-        
-        
+        # Downloadable csv of selected dataset 
         output$downloadData <- downloadHandler(
             filename = function() {
                 selected <-c()
@@ -165,7 +164,4 @@ function(input, output) {
                 write.csv(data, con, row.names = TRUE)
             }
         )
-        
-   
-    
 }
--- ui.R
+++ ui.R
@@ -4,10 +4,11 @@ library(shiny)
 library(shinyWidgets)
 
 fluidPage(
-    titlePanel("World Happiness Reports"),
+    #text with project name and my information
+    titlePanel("World Happiness Report"),
     tags$h3("DS 3002- Project One"),
     tags$h4("Elit Dogu, ejd5mm 3rd Year UVA"),
-    # use a gradient in background
+    # use a gradient in background, setting background color to blue
     setBackgroundColor(  
         #https://rdrr.io/cran/shinyWidgets/man/setBackgroundColor.html used this website for help on background color
         color = c("#F7FBFF", "#2171B5"),
@@ -24,47 +25,47 @@ fluidPage(
             h4("Summary"),
             verbatimTextOutput("summary"),
             
-            # Button
+            # Download button
             downloadButton("downloadData", "Download")
         ),
     # Create a new Row in the UI for selectInputs
     # Main panel for displaying outputs ----
     mainPanel(
-        
-    fluidRow(
+
+    fluidRow(  #manipulates the original dataframe given user selection
         column(4,
-               selectInput("year",
+               selectInput("year",  #selection for the year variable
                            "Year:",
                            c("All",
                              unique(as.numeric(df$Year))))
         ),
         column(4,
-               selectInput("country",
+               selectInput("country",  #selection for the country variable
                            "Country:",
                            c("All",
                              unique(as.character(df$Country))))
         ),
         column(4,
-               selectInput("continent",
+               selectInput("continent",  #selection for the continent variable
                            "Continent:",
                            c("All",
                              unique(as.character(df$Continent))))
         )
     ),
-    # Create a new row for the table.
+    # Create a new row for the table
     DT::dataTableOutput("table"),
-    column(12,
-           verbatimTextOutput("columns")
-    ),
-    column(12,
-           verbatimTextOutput("rows")
-    ),
-
-    column(12,
-           verbatimTextOutput("data_ex")
-    )
     
-   )
-    )
+    # Create a new column for the text to be displayed
+    column(12,
+           verbatimTextOutput("columns") #column to display col count
+    ),
+    column(12,
+           verbatimTextOutput("rows") #column to display row count
+    ),
 
+    column(12,
+           verbatimTextOutput("data_ex") #column to display more information text
+            )
+        )
+    )
 )
